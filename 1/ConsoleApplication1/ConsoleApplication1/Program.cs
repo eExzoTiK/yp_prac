a07f1c5889e69ccab247daad2aa3219fa4dd8afa
Подложный Кирилл Владимирович <316215@college.local>
2019-04-16 11:55:23 +0300

    com

--- 1/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ 1/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -4,12 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication1
+namespace ConsoleApplication2
 {
+    class Receipt
+    {
+        public int Number;
+        public string Date;
+        public float Sum;
+
+        public Receipt()
+        {
+            Number = 1;
+            Date = "15.04.2019";
+            Sum = 50.0f;
+        }
+
+        public void Add()
+        {
+            Console.WriteLine("Номер квитанции: " + Number);
+            Console.WriteLine("Дата: " + Date);
+            Console.WriteLine("Сумма: " + Sum);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Receipt first = new Receipt();
+
+            first.Add();
+
+            Console.Read();
         }
     }
 }
